c0f0ad7796226618c7b9c0252e463d37069cc84d
Félix Prudhomme <felixprudhomme@yahoo.com>
2026-02-13 09:43:12 +0100

    fix: not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function notFound() {
         <main id="page entière" className="flex flex-col flex-1 p-5 bg-cloud overflow-auto">
             <div id="article" className="flex flex-col">
                 <h1 id="title" className="text-center Horizon text-3xl pb-5">Page introuvable</h1>
-                <p id="presentation" className="text-justify">
+                <p id="content" className="self-center pb-5">
                     Désolé, la page que vous recherchez n'existe pas.
                 </p>
                 <Link href="/" className="self-center justify-center">
